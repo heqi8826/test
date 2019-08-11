98e6d9f78f4aae77157c4dff25c673c6c355f924
heqi8826 <heqi8826@gmail.com>
2019-08-12 00:28:52 +0800

    更新练习： 准备训练集、测试集、验证集完毕。 删除mytf文件夹下的索引文件（index.html）

--- mytf/heart_disease_prediction.py
+++ mytf/heart_disease_prediction.py
@@ -1,42 +1,24 @@
-import tensorflow as tf
-from tensorflow import keras
+from __future__ import absolute_import, division, print_function, unicode_literals
+
 import numpy as np
 import pandas as pd
+
+import tensorflow as tf
+from tensorflow import feature_column
+from tensorflow.keras import layers
 from sklearn.model_selection import train_test_split
 
 # 下载心脏病例数据集
-Url = "https://storage.googleapis.com/applied-dl/heart.csv"
-heart_sill_form = pd.read_csv(Url)
-
+URL = 'https://storage.googleapis.com/applied-dl/heart.csv'
+dataframe = pd.read_csv(URL)  # 数据类型：pandas.core.frame.DataFrame
+print(dataframe)
+print(dataframe.index)
+print(dataframe.columns)
+print(dataframe.to_numpy())
 
 # 将数据集切割为：训练集、验证集、测试集
-train, test = train_test_split(heart_sill_form, test_size=0.2)
+train, test = train_test_split(dataframe, test_size=0.2)
 train, val = train_test_split(train, test_size=0.2)
-# print(len(train), 'train examples')
-# print(len(val), 'validation examples')
-# print(len(test), 'test examples')
-
-
-# A utility method to create a tf.data dataset from a Pandas Dataframe
-def df_to_dataset(heart_sill_form, shuffle=True, batch_size=32):
-  heart_sill_form = heart_sill_form.copy()
-  labels = heart_sill_form.pop('target')
-  ds = tf.data.Dataset.from_tensor_slices((dict(heart_sill_form), labels))
-  if shuffle:
-    ds = ds.shuffle(buffer_size=len(heart_sill_form))
-  ds = ds.batch(batch_size)
-  return ds
-
-
-batch_size = 5  # A small batch sized is used for demonstration purposes
-train_ds = df_to_dataset(train, batch_size=batch_size)
-val_ds = df_to_dataset(val, shuffle=False, batch_size=batch_size)
-test_ds = df_to_dataset(test, shuffle=False, batch_size=batch_size)
-
-for feature_batch, label_batch in train_ds.take(1):
-  print('Every feature:', list(feature_batch.keys()))
-  print('A batch of ages:', feature_batch['age'])
-  print('A batch of targets:', label_batch)
-
-
-
+print(len(train), 'train examples', type(train))
+print(len(val), 'validation examples', type(val))
+print(len(test), 'test examples', type(test))
