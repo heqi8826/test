2aa806b21e542c60dca4ce7589496c89d14abcd7
heqi8826 <heqi8826@gmail.com>
2019-08-12 18:21:36 +0800

    线性回归问题代码 存在处理 numpy ：points 数据错误

--- mytf/lineloss.py
+++ mytf/lineloss.py
@@ -1,52 +1,68 @@
-import tensorflow as tf
-from tensorflow import keras
 import numpy as np
-import pandas as pd
 
 # 导入数据集
-url = './data.csv'
-dataframe = pd.read_csv(url).drop(columns=['Unnamed: 0', 'Unnamed: 3', 'Unnamed: 4', 'Unnamed: 5', 'Unnamed: 6'])
-mydict = pd.DataFrame(dataframe)
-points = mydict.to_numpy()
+points = np.genfromtxt('data.csv', delimiter=',')
+print(len(points[:, 0]))
 
 
-# 定义函数，计算线性模型y=wx+b的loss均方误差平均值
+# 计算loss损失
 def myloss(points, w, b):
-    losserror = 0
-    for i in range(len(points)):
+    loss_sum = 0
+
+    for i in range(len(points[:, 0])):
         x = points[i][0]
         y = points[i][1]
-        losserror += ((w * x + b) - y) ** 2
-        return losserror / len(points)
+        loss_sum += (w * x + b - y) ** 2
+        return loss_sum / len(points[:, 0])
 
 
-# 计算w' 和 b'
-def my_w_b(w_current, b_current, points, learningrate):
-    w = 0
-    b = 0
-    for i in range(len(points)):
-        x = points[i][0]
-        y = points[i][1]
-        N = len(points)
-        # w 和 b 的变化率累加
-        w += 2/N * (w_current * x + b_current - y) * x
-        b += 2/N * (w_current * x + b_current - y)
-        return w, b
-    new_w = w_current - learningrate * w
-    new_b = b_current - learningrate * b
+# 计算 w b
+def mygradient(points, w_current, b_current, lr):
+    w_gradient = 0
+    b_gradient = 0
+    N = len(points[:, 0])
+    for i in range(len(points[:, 0])):
+        # print(len(points[:, 0])) 测试打印长度临时使用
+        x = points[i, 0]
+        y = points[i, 1]
+        # grad_w = 2(wx+b-y)*x
+        w_gradient += (2/N) * x * ((w_current * x + b_current) - y)
+        # grad_b = 2(wx+b-y)
+        b_gradient += (2/N) * ((w_current * x + b_current) - y)
+    new_w = w_current - lr * w_gradient
+    new_b = b_current - lr * b_gradient
     return [new_w, new_b]
 
-def myrun(points, b, w, num):
-    b = b
-    w = w
-    for i in range(num):
-        b, w = my_w_b(b, w, mypoints, learningrate)
-    return [b, w]
+
+# 最佳w 和 b 预测
+def best_w_b(points, w_start, b_start, num, lr):
+    w = w_start
+    b = b_start
+    # 迭代数次 得出最佳w b
+    for i in range(int(num)):
+        [w, b] = mygradient(points, w, b, lr)
+        return [w, b]
 
 
+# 运行函数runner
+def myrunner():
+    lr = 0.0001
+    initial_b = 0  # initial y-intercept guess
+    initial_w = 0  # initial slope guess
+    num = 1000
+    print("Starting gradient descent at b = {0}, w = {1}, error = {2}"
+          .format(initial_b, initial_w,
+                  myloss(points, initial_w, initial_b))
+          )
+    print("Running...")
+    [w, b] = best_w_b(points, initial_w, initial_b, num, lr)
+    print("After {0} iterations  w = {2}, b = {1}, error = {3}".
+          format(num, w, b,
+                 myloss(b, w, points))
+          )
 
 
-
+myrunner()
 
 
 
