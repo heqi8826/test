15950a57b27ffd6fe0793a11ba4f628d7663dc39
heqi8826 <heqi8826@gmail.com>
2019-08-20 12:09:44 +0800

    no message

--- tf基本语法/前向传播.py
+++ tf基本语法/前向传播.py
@@ -17,6 +17,30 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 # 数据转换为tensor类型 ，定义数据为tf的浮点型32位数据
 x_train = tf.convert_to_tensor(x_train, dtype=tf.float32) / 255.  # 除以255. 为了方便计算 将[0,255]范围转换为[0,1]范围
 y_train = tf.convert_to_tensor(y_train, dtype=tf.int32)
-print(x_train.shape, y_train.shape, x_train.dtype, y_train.dtype)
+# print(x_train.shape, y_train.shape, x_train.dtype, y_train.dtype)
 # keras方法 查看最大值 最小值 reduce_min reduce_max
-print(tf.reduce_max(x_train), '\n',  tf.reduce_min(x_train), '\n', tf.reduce_min(y_train), '\n', tf.reduce_max(y_train))
+# print(tf.reduce_max(x_train),'\n',tf.reduce_min(x_train),'\n',tf.reduce_min(y_train),'\n',tf.reduce_max(y_train))
+# 因原始数据较大，将其进行切片 创建数据集 以每次一个小批量（128）个进行训练计算。
+train_db = tf.data.Dataset.from_tensor_slices((x_train, y_train)).batch(128)
+# 迭代 属性类型为：tensorflow.python.data.ops.iterator_ops.IteratorV2 object
+train_iter = iter(train_db)
+# 不断next查看 next_train的tensor数据
+next_train = next(train_iter)
+# 下面的构造W 和 b 的原理是 降维过程
+# [b,784] => [b,256] => [b,128] => [b,10]
+# 即：w：[dim_in,dim_out]  b：[dim_out]
+w1 = tf.random.truncated_normal([784, 256])  # truncated 截断意思
+# 第一层原始x_train的 dim_in 为784 输入，输出设计为 [b,256] 故 dim_out 为256 所以W1 构造为：[784，256]
+b1 = tf.zeros([256])
+w2 = tf.random.truncated_normal([256, 128])
+b2 = tf.zeros([128])
+w3 = tf.random.truncated_normal([128, 10])
+b3 = tf.zeros([10])
+# 下一步迭代训练数据（x_train，y_train）到模型层y=x@w+b
+for (x_train, y_train) in train_db:
+    # x_train:[128,28,28] y_train:[128]
+    tf.reshape(x_train, [-1, 28*28])
+
+# 视频课件第四章后三个视频内容。
+
+
---
+++ tf高阶/合并与分割.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+from tensorflow import keras
+'''
+tf.concat 合并  tf.split 切割 tf.stack 堆叠  tf.unstak
+'''
+
+a = tf.ones([4, 35, 8])
+b = tf.ones([2, 35, 8])
+# tf.concat拼接合并两个tensor a 与 b ,concat 内有两个参数 合并的两个tensor 和 合并的纬度索引值
+c = tf.concat([a, b], axis=0)
+print(c.shape)
+a = tf.ones([4, 32, 8])
+b = tf.ones([4, 3, 8])
+d = tf.concat([a, b], 1)
+print(d.shape)
+
+# stack 创建新dim纬度，增加纬度 把两个tensor堆叠起来
+a = tf.ones([4, 35, 8])
+b = tf.ones([4, 35, 8])
+e = tf.stack([a, b], 0)  # a,b 所有纬度相同
+print(e.shape)
+
+# unstak 指定 一个纬度 在此纬度上 全部打散
+a = tf.ones([4, 35, 8])
+aa = tf.unstack(a, axis=2)
+# 在 索引为2的纬度上 打散数据 也就是生成了8个【4，35】的列表
+print(len(aa))
+# 打印数字为 8
+print(aa[0].shape, '\n', aa[7].shape)
+b = tf.ones([3, 4, 5, 3])
+bb = tf.unstack(b, num=3, axis=3)
+print(b[0].shape, b[0])
+
+
+# diy式的打散数据 在某个纬度上 使用split
+c = tf.ones([2, 4, 35, 8])
+cc = tf.split(c, num_or_size_splits=2)  # 均分
+print(len(cc))
+ccc = tf.split(c, num_or_size_splits=[2, 2, 4], axis=3)  # 按照2：2：4
+print(len(ccc), '\n', ccc[0].shape, '\n', ccc[1].shape, '\n', ccc[2].shape)
+'''
+3 
+ (2, 4, 35, 2) 
+ (2, 4, 35, 2) 
+ (2, 4, 35, 4)
+'''
