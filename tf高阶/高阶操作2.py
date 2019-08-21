26674793e3d0690586b4ca0eb7a817768f9de449
heqi8826 <heqi8826@gmail.com>
2019-08-21 18:12:08 +0800

    gaojie2报错 tensorflow.python.framework.errors_impl.InvalidArgumentError: cannot compute Mul as input #1(zero-based) was expected to be a double tensor but is a float tensor [Op:Mul] name: meshgrid/mul/

--- tf高阶/高阶操作2.py
+++ tf高阶/高阶操作2.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-
+import numpy as np
 
 update = tf.random.normal([2, 4, 4])
 shape = tf.constant([4, 4, 4])
@@ -8,3 +8,22 @@ indices = tf.constant([[0], [2]])
 
 res = tf.scatter_nd(indices, update, shape)
 print(res)
+
+points = []
+for x in np.linspace(-2, 2, 5):
+    for y in np.linspace(-2, 2, 5):
+        z = [x, y]
+        points.append(z)
+        np.array(points)
+
+print(len(points), '\n', points)
+
+# 用另外一种便捷方法：meshgrid
+x = tf.linspace(-2., 2, 5)
+y = np.linspace(-2., 2, 5)
+y = tf.convert_to_tensor(y)
+x1, y1 = tf.meshgrid(x, y)
+print(x1.shape)
+
+
+
