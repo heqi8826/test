8e6141c582ed282a64d440915312c4c68f05474c
heqi8826 <heqi8826@gmail.com>
2019-08-22 10:44:36 +0800

    no message

--- tf高阶/高阶操作2.py
+++ tf高阶/高阶操作2.py
@@ -20,10 +20,20 @@ print(len(points), '\n', points)
 
 # 用另外一种便捷方法：meshgrid
 x = tf.linspace(-2., 2, 5)
+x = tf.cast(x, dtype=tf.double)
 y = np.linspace(-2., 2, 5)
 y = tf.convert_to_tensor(y)
-x1, y1 = tf.meshgrid(x, y)
-print(x1.shape)
+points_x, points_y = tf.meshgrid(x, y)
+print(points_x.shape, points_x)
+print(points_y.shape, points_y)
+print('-----')
+points = tf.stack([points_x, points_y], axis=0)
+print('当axis=0时候拼接结果为：', points)
 
+points = tf.stack([points_x, points_y], axis=1)
+print('当axis=1时候拼接结果为：', points)
+
+points = tf.stack([points_x, points_y], axis=2)
+print('当axis=2时候拼接结果为：', points)
 
 
---
+++ 加载数据/datasets.py
@@ -0,0 +1,78 @@
+import tensorflow as tf
+from tensorflow import keras
+'''
+小型数据集
+keras.datasets
+tf.data.Dataset.from_tensor_slices
+    shuffle
+    map
+    batch
+    repeat
+常用数据集
+boston housing
+mnist/fashion mnist
+cifar10/100 10大类 每大类对应10个小类
+imdb
+'''
+# (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()  # 拿到的数据为numpy数据 非tensor数据
+# print(x_train.shape, '\n', y_train.shape, '\n', x_train.min(), '\n', x_train.max(), '\n', x_train.mean())
+# 上述打印所使用的min max mean的方法为numpy工具包中的 ，如果为tensor数据 则使用tf.reduce_min / reduce.max /reduce.mean
+'''
+(60000, 28, 28) 
+ (60000,)  # 是一个0-9的列表，后续编码为one-hot的tensor数据，one-hot占用资源多 前期不做转换。
+ 0 
+ 255  # 后续为方便处理 可以令x_train 除以 255 变为 0-1范围，根据预处理需求也可以变为-1～1范围 或者0-1范围等等diy范围。
+ 33.318421449829934
+'''
+# print(x_test.shape, '\n', y_test.shape, '\n', x_test.max())
+'''
+(10000, 28, 28) 
+ (10000,)
+ 255
+'''
+# 转换y_train为one_hot编码
+# 首先查看下内部数据形式 ，任意取n个数据查看
+# test_y = y_train[0:5]  # pick five
+# print(test_y, test_y.dtype)  # [5 0 4 1 9]
+# y_train = tf.one_hot(y_train, depth=10)  # 独热编码转换，数据类型转换为了float32
+# print(y_train[0:5])
+'''
+tf.Tensor(
+[[0. 0. 0. 0. 0. 1. 0. 0. 0. 0.]
+ [1. 0. 0. 0. 0. 0. 0. 0. 0. 0.]
+ [0. 0. 0. 0. 1. 0. 0. 0. 0. 0.]
+ [0. 1. 0. 0. 0. 0. 0. 0. 0. 0.]
+ [0. 0. 0. 0. 0. 0. 0. 0. 0. 1.]], shape=(5, 10), dtype=float32)
+'''
+
+# 使用cifar10/100 加载数据集中的小像素图片
+(x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
+# 转换成tensor数据类型
+db = tf.data.Dataset.from_tensor_slices(x_test)
+
+# 通过迭代器将db一个一个数据取样
+one = next(iter(db)).shape
+print(one)
+db = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+print(next(iter(db))[0].shape)
+
+# .shuffle 方法 打散数据
+db = db.shuffle(buffer_size=10000)
+
+
+def preprocess(x, y):
+    x = tf.cast(x, dtype=tf.float32)/255
+    y = tf.cast(y, dtype=tf.int32)
+    y = tf.one_hot(y, depth=10)
+    return x, y
+
+
+db2 = db.map(preprocess)
+res = next(iter(db2))
+print(res[0].shape, res[1].shape)
+print(res[1][:2])
+# .batch 方法
+db3 = db2.batch(32)
+res = next(iter(db3))
+print(res[0].shape, res[1].shape)
